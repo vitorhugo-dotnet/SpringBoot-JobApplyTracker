5ff58adec7af1188fdc69330254a16190f304501
Vitor Hugo <vitorhugoalvesferreira@gmail.com>
2026-05-28 22:22:21 -0300

    Force embedded test database schema generation for integration tests

--- src/test/java/com/jobtracker/integration/AbstractIntegrationTest.java
+++ src/test/java/com/jobtracker/integration/AbstractIntegrationTest.java
@@ -4,7 +4,20 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@SpringBootTest(
+        webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+        properties = {
+                "spring.datasource.url=jdbc:h2:mem:jobtracker_test;MODE=MySQL;DATABASE_TO_LOWER=TRUE;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE",
+                "spring.datasource.driver-class-name=org.h2.Driver",
+                "spring.datasource.username=sa",
+                "spring.datasource.password=",
+                "spring.jpa.hibernate.ddl-auto=create-drop",
+                "spring.jpa.show-sql=false",
+                "spring.jpa.properties.hibernate.dialect=org.hibernate.dialect.H2Dialect",
+                "spring.flyway.enabled=false",
+                "spring.sql.init.mode=never"
+        }
+)
 @AutoConfigureMockMvc
 @ActiveProfiles("test")
 public abstract class AbstractIntegrationTest {
